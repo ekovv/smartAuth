78cc4f31ce87b8d5a01a94f4fd31fe50b50956e6
ekovv <denisovdima2004@yandex.ru>
2024-02-04 22:28:00 +0300

    :tada: start grpc ✅

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,9 +1,46 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+	"smartAuth/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
 func main() {
-	// TODO: инициализировать обьект конфига
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+	log.Info("starting app",
+		slog.Any("cfg", cfg))
+
+	log.Debug("debug message")
+
 	// TODO: инициализировать логгер
 	// TODO: инициализировать приложение (app)
 	// TODO: запустить grpc сервер приложения
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
