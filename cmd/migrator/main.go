ebee131fdb42154e8057dad5b3ffdfe58364ed6f
ekovv <denisovdima2004@yandex.ru>
2024-02-12 15:30:21 +0300

    :tada: build :sparkles:

--- cmd/migrator/main.go
+++ cmd/migrator/main.go
@@ -19,9 +19,11 @@ func main() {
 	if storagePath == "" {
 		panic("storage path is required")
 	}
+
 	if migrationsPath == "" {
 		panic("migrations path is required")
 	}
+
 	m, err := migrate.New(
 		"file://"+migrationsPath, fmt.Sprintf("sqlite3://%s?x-migrations-table=%s", storagePath, migrationsTable))
 	if err != nil {
